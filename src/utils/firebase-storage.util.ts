8617541639045f53384e744d90223a852451876a
Esmail Elmoussel <elmoussel12@gmail.com>
2022-04-11 20:18:38 +0200

    [uploadFile]: add onProgress and onComplete callback funstions

--- src/utils/firebase-storage.util.ts
+++ src/utils/firebase-storage.util.ts
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  StorageError,
+} from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,12 +18,43 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-export const firebaseStorage = getStorage(firebaseApp);
+const firebaseStorage = getStorage(firebaseApp);
 
-export const uploadFile = async (file: File) => {
-  const storageRef = ref(firebaseStorage, "file.png");
+export const uploadFile = async ({
+  file,
+  onProgress,
+  onComplete,
+  onError,
+}: {
+  file: File;
+  onProgress?: (progress: number) => void;
+  onComplete?: (fileUrl: string) => void;
+  onError?: (err: StorageError) => void;
+}) => {
+  const storageRef = ref(
+    firebaseStorage,
+    new Date().getTime() + "-" + file.name
+  );
 
-  return await uploadBytes(storageRef, file).then((snapshot) => {
-    return snapshot;
-  });
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      onProgress && onProgress(progress);
+    },
+    (error) => {
+      if (onError) {
+        return onError(error);
+      }
+
+      console.error("Failed to upload file");
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        onComplete && onComplete(downloadURL);
+      });
+    }
+  );
 };
