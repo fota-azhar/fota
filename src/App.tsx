24c68453e1ab68e0684f9eb1f09a3aaecc98797b
Esmail Elmoussel <elmoussel12@gmail.com>
2022-06-25 01:58:47 +0200

    feat: upload file to firebase storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createUpdate } from "./utils/firebase/database.util";
+import { uploadFile } from "./utils/firebase/storage.util";
 
 function App() {
+  const [image, setImage] = useState<File | undefined>(undefined);
+
+  const upload = async () => {
+    if (!image) {
+      return;
+    }
+
+    uploadFile({
+      file: image,
+      onProgress: (progress) => {
+        console.log({ progress });
+      },
+      onComplete: async (fileUrl) => {
+        createUpdate(fileUrl);
+      },
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <input
+          type="file"
+          onChange={(e) => {
+            const file = e?.target?.files?.[0];
+            setImage(file);
+          }}
+        />
+        <button onClick={upload}>Upload</button>
       </header>
     </div>
   );
