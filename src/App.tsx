8dd864637a1d8fde392d85d1eb24ca46ed64539e
Esmail Elmoussel <elmoussel12@gmail.com>
2022-07-12 23:15:50 +0200

    feat(auth): create login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,15 @@
-import { useState } from "react";
 import "./App.css";
-import { createUpdate } from "./utils/firebase/database.util";
-import { Button, Typography } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
-import { HexFile } from "./types";
+import { useState } from "react";
+import { Home, Login } from "./pages";
 
 function App() {
-  const [hexFile, setHexFile] = useState<HexFile | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [isAuth, setIsAuth] = useState(false);
 
-  const upload = async () => {
-    if (!hexFile) {
-      return;
-    }
+  if (!isAuth) {
+    return <Login onSuccess={() => setIsAuth(true)} />;
+  }
 
-    setLoading(true);
-
-    await createUpdate(hexFile);
-
-    setHexFile(null);
-    setLoading(false);
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Typography>{hexFile?.name}</Typography>
-        <Button variant="contained" component="label">
-          Upload File
-          <input
-            type="file"
-            hidden
-            accept=".hex"
-            onChange={(e) => {
-              const hexFile = e?.target?.files?.[0];
-              if (!hexFile) {
-                return;
-              }
-
-              const reader = new FileReader();
-
-              reader.readAsText(hexFile, "UTF-8");
-
-              reader.onload = function (event) {
-                const fileContent = event?.target?.result
-                  ?.toString()
-                  .replaceAll("\n", "")
-                  .replaceAll(" ", "")
-                  .replaceAll(":", "");
-
-                if (fileContent) {
-                  setHexFile({ name: hexFile.name, content: fileContent });
-                }
-              };
-            }}
-          />
-        </Button>
-
-        <LoadingButton
-          variant="contained"
-          onClick={upload}
-          disabled={!hexFile}
-          loading={loading}
-        >
-          Upload
-        </LoadingButton>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ src/pages/login.page.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export function Login({ onSuccess }: { onSuccess: () => void }) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const savedUsername = process.env.REACT_APP_USERNAME?.toLocaleLowerCase();
+    const savedPassword = process.env.REACT_APP_PASSWORD;
+
+    const enteredUsername = data
+      .get("username")
+      ?.toString()
+      .toLocaleLowerCase();
+    const enteredPassword = data.get("password");
+
+    if (
+      enteredUsername !== savedUsername ||
+      savedPassword !== enteredPassword
+    ) {
+      return alert("Wrong credentials");
+    }
+
+    onSuccess();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
